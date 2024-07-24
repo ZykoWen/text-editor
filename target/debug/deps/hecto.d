77f4ac32ce5ba960d6915ca2e5147febfefd3bc1
Zyko <2049973245@qq.com>
2024-07-24 21:15:24 +0800

    create a struct

--- target/debug/deps/hecto.d
+++ target/debug/deps/hecto.d
@@ -1,5 +1,6 @@
-D:\rustProject\hecto\target\debug\deps\hecto.exe: src/main.rs
+D:\rustProject\hecto\target\debug\deps\hecto.exe: src/main.rs src\editor.rs
 
-D:\rustProject\hecto\target\debug\deps\hecto.d: src/main.rs
+D:\rustProject\hecto\target\debug\deps\hecto.d: src/main.rs src\editor.rs
 
 src/main.rs:
+src\editor.rs:
--- target/debug/hecto.d
+++ target/debug/hecto.d
@@ -1,1 +1,1 @@
-D:\rustProject\hecto\target\debug\hecto.exe: D:\rustProject\hecto\src\main.rs
+D:\rustProject\hecto\target\debug\hecto.exe: D:\rustProject\hecto\src\editor.rs D:\rustProject\hecto\src\main.rs
