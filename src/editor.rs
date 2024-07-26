1cfeab476b0c8e59d2b39496c928c5ba824b013f
Zyko <2049973245@qq.com>
2024-07-26 09:52:31 +0800

    add panic!

--- src/editor.rs
+++ src/editor.rs
@@ -1,30 +1,30 @@
 #![warn(clippy::all, clippy::pedantic)]
 use crossterm::event::{read,Event::Key,KeyCode::Char};
 use crossterm::terminal::{enable_raw_mode,disable_raw_mode};
-pub struct Editor{
-
-}
+pub struct Editor{}
 impl Editor{
     pub fn default()->Self{
         Editor{}
     }
-    pub fn run(&self){
-        enable_raw_mode().unwrap();
-        loop{
-            match read(){
-                Ok(Key(event))=>{
-                    println!("{event:?} \r");
-                    if let Char(c)=event.code{
-                        if c == 'q'{
-                            break;
-                        }
+    pub fn run(&self) {
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
+        }
+        println!("Goodbye");
+    }
+    fn repl(&self)->Result<(),std::io::Error> {
+        enable_raw_mode()?;
+        loop {
+            if let Key(event )=read()? {
+                println!("{event:?} \r");
+                if let Char(c)=event.code{
+                    if c=='q' {
+                        break;
                     }
-                },
-                Err(err)=>println!("Error:{err}"),
-                _=>()
+                }
             }
         }
-        disable_raw_mode().unwrap();
+        disable_raw_mode()?;
+        Ok(())
     }
-
 }
