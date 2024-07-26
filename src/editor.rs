461168cceae0e7aae0387742217264088c318ea6
Zyko <2049973245@qq.com>
2024-07-26 10:53:19 +0800

    add should_quit into Editor

--- src/editor.rs
+++ src/editor.rs
@@ -1,27 +1,33 @@
 #![warn(clippy::all, clippy::pedantic)]
-use crossterm::event::{read,Event::Key,KeyCode::Char};
+use crossterm::event::{read,Event::Key,KeyCode::Char,KeyEvent,KeyModifiers};//添加后两个
 use crossterm::terminal::{enable_raw_mode,disable_raw_mode};
-pub struct Editor{}
+pub struct Editor{
+    should_quit:bool  //增加元素，用于判断是否需要退出循环
+}
 impl Editor{
     pub fn default()->Self{
-        Editor{}
+        Editor{should_quit:false}
     }
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         if let Err(err) = self.repl() {
             panic!("{err:#?}");
         }
         println!("Goodbye");
     }
-    fn repl(&self)->Result<(),std::io::Error> {
+    fn repl(&mut self)->Result<(),std::io::Error> {
         enable_raw_mode()?;
         loop {
-            if let Key(event )=read()? {
-                println!("{event:?} \r");
-                if let Char(c)=event.code{
-                    if c=='q' {
-                        break;
+            if let Key(KeyEvent{code,modifiers,kind,state})=read()?{
+                println!("Code:{code:?},Modifiers:{modifiers:?},Kind:{kind:?},State:{state:?}");
+                match code {
+                    Char('q') if modifiers==KeyModifiers::CONTROL=>{
+                        self.should_quit=true;  //当遇到Ctrl+q时，终止程序
                     }
+                    _=>()
                 }
+            }
+            if self.should_quit==true{
+                break;
             }
         }
         disable_raw_mode()?;
