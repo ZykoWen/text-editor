3571bc900611292ac533d0c95d292febd46f3b49
Zyko <2049973245@qq.com>
2024-08-01 11:15:07 +0800

    add more clean logic

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,8 @@
 #![warn(clippy::all, clippy::pedantic)]
-use crossterm::event::{read,Event::Key,KeyCode::Char,KeyEvent,KeyModifiers};//添加后两个
-use crossterm::terminal::{enable_raw_mode,disable_raw_mode};
+use crossterm::event::{read,Event,Event::Key,KeyCode::Char,KeyEvent,KeyModifiers};//添加后两个
+use crossterm::execute;
+use std::io::stdout;
+use crossterm::terminal::{enable_raw_mode,disable_raw_mode,Clear,ClearType};
 pub struct Editor{
     should_quit:bool  //增加元素，用于判断是否需要退出循环
 }
@@ -9,28 +11,50 @@ impl Editor{
         Editor{should_quit:false}
     }
     pub fn run(&mut self) {
-        if let Err(err) = self.repl() {
-            panic!("{err:#?}");
-        }
-        println!("Goodbye");
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+    fn initialize() -> Result<(),std::io::Error>{
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+    fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
+    }
+    fn clear_screen() -> Result<(),std::io::Error>{
+        let mut stdout = stdout();//stdout() 是调用 stdout 函数，这个函数返回一个 Stdout 类型的值，表示标准输出流。
+        execute!(stdout,Clear(ClearType::All))//execute! 是一个宏（macro），用于执行终端相关的操作。stdout 是传递给 execute! 宏的参数，表示要操作的标准输出流。
+        // Clear(ClearType::All) 是传递给 execute! 宏的另一个参数，表示要执行的操作是清空屏幕。ClearType::All 是一个枚举值，表示清空整个屏幕。
+
     }
     fn repl(&mut self)->Result<(),std::io::Error> {
-        enable_raw_mode()?;
         loop {
-            if let Key(KeyEvent{code,modifiers,kind,state})=read()?{
-                println!("Code:{code:?},Modifiers:{modifiers:?},Kind:{kind:?},State:{state:?}");
-                match code {
-                    Char('q') if modifiers==KeyModifiers::CONTROL=>{
-                        self.should_quit=true;  //当遇到Ctrl+q时，终止程序
-                    }
-                    _=>()
-                }
-            }
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
             if self.should_quit==true{
                 break;
             }
         }
-        disable_raw_mode()?;
+        Ok(())
+    }
+    fn evaluate_event(&mut self,event:&Event){
+        if let Key(KeyEvent{code,modifiers,..})=event{
+            match code{
+                Char('q') if *modifiers == KeyModifiers::CONTROL =>{
+                    self.should_quit = true;
+                }
+                _ => (),
+            }
+        }
+    }
+    fn refresh_screen(&self)->Result<(),std::io::Error>{
+        if self.should_quit{
+            Self::clear_screen()?;
+            print!("Googbye.\r\n");
+        }
         Ok(())
     }
 }
