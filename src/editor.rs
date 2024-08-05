8b062b993fbe0251b1d79243493fb7fd7e1c0294
Zyko <2049973245@qq.com>
2024-08-05 10:16:24 +0800

    assignment3--my way

--- src/editor.rs
+++ src/editor.rs
@@ -2,17 +2,18 @@
 
 use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
 mod terminal;
-use terminal::{Terminal,Position};
+use std::io::Error;
+use terminal::{Position, Terminal};
 
-pub struct Editor{
-    should_quit:bool  //增加元素，用于判断是否需要退出循环
+pub struct Editor {
+    should_quit: bool, //增加元素，用于判断是否需要退出循环
 }
-impl Editor{
-    pub const fn default()->Self{
+impl Editor {
+    pub const fn default() -> Self {
         //下面函数中仅仅定义了一个结构体，所以可以将其设置为const函数，
         // It enables this function to be evaluated on compile time.
         // Editor{should_quit:false}
-        Self{should_quit:false}//不用再次重复结构体名称，同时可以避免之后改变结构体名字，此处还使用原名称
+        Self { should_quit: false } //不用再次重复结构体名称，同时可以避免之后改变结构体名字，此处还使用原名称
     }
     pub fn run(&mut self) {
         //三个位置可能出现error：初始化、实现步骤、关闭步骤
@@ -21,7 +22,7 @@ impl Editor{
         Terminal::terminate().unwrap();
         result.unwrap();
     }
-    pub fn repl(&mut self)->Result<(),std::io::Error> {
+    pub fn repl(&mut self) -> Result<(), Error> {
         loop {
             // let event = read()?;
             // self.evaluate_event(&event);
@@ -38,35 +39,41 @@ impl Editor{
         }
         Ok(())
     }
-    pub fn evaluate_event(&mut self,event:&Event){
-        if let Key(KeyEvent{code,modifiers,..})=event{
-            match code{
-                Char('q') if *modifiers == KeyModifiers::CONTROL =>{
+    pub fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
                     self.should_quit = true;
                 }
                 _ => (),
             }
         }
     }
-    pub fn refresh_screen(&self) -> Result<(),std::io::Error>{
+    pub fn refresh_screen(&self) -> Result<(), Error> {
         Terminal::hide_cursor()?;
-        if self.should_quit == true{
+        if self.should_quit == true {
             Terminal::clear_screen()?;
             Terminal::print("Goodbye!")?;
-        }else{
+        } else {
             Self::draw_rows()?;
-            Terminal::move_cursor_to(Position{x:0,y:0})?;
+            Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
         }
         Terminal::show_cursor()?;
         Terminal::execute()?;
         Ok(())
     }
-    pub fn draw_rows() -> Result<(),std::io::Error>{
+    pub fn draw_rows() -> Result<(), Error> {
         let height = Terminal::size()?.height;
-        for current_row in 0..height{
-            Terminal::clear_line()?;//先清理当前行，再写波浪线
+        for current_row in 0..height {
+            Terminal::clear_line()?; //先清理当前行，再写波浪线
             Terminal::print("~")?;
-            if current_row+1 < height{
+            if current_row + 1 == (height / 3 as u16) {
+                Terminal::print_center("hecto editor --version 0.1.0", (height / 3 as u16))?;
+            }
+            if current_row + 1 < height {
                 Terminal::print("\r\n")?;
             }
         }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,26 +1,26 @@
-use std::io::stdout;
-use std::io::Write;
-use std::io::Error;
+use crossterm::cursor::{Hide, MoveTo, Show};
 use crossterm::queue;
-use crossterm::cursor::{MoveTo, Hide, Show};
 use crossterm::style::Print;
-use crossterm::terminal::{enable_raw_mode,disable_raw_mode,Clear,ClearType,size};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
+use std::io::stdout;
+use std::io::Error;
+use std::io::Write;
 
 pub struct Terminal; //无字段的结构体
-pub struct Size{
-    pub height:u16,
-    pub width:u16,
+pub struct Size {
+    pub height: u16,
+    pub width: u16,
 }
 #[derive(Copy, Clone)]
-pub struct Position{
-    pub x:u16,
-    pub y:u16,
+pub struct Position {
+    pub x: u16,
+    pub y: u16,
 }
-impl Terminal{
-    pub fn initialize() -> Result<(),Error>{
+impl Terminal {
+    pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(Position{x:0,y:0})?;
+        Self::move_cursor_to(Position { x: 0, y: 0 })?;
         Self::execute()?;
         Ok(())
     }
@@ -29,37 +29,43 @@ impl Terminal{
         disable_raw_mode()?;
         Ok(())
     }
-    pub fn clear_screen() -> Result<(),Error>{
-        queue!(stdout(),Clear(ClearType::All))?;
+    pub fn clear_screen() -> Result<(), Error> {
+        queue!(stdout(), Clear(ClearType::All))?;
         Ok(()) //Clear(ClearType::All) 是传递给 execute! 宏的另一个参数，表示要执行的操作是清空屏幕。ClearType::All 是一个枚举值，表示清空整个屏幕。
     }
-    pub fn clear_line() -> Result<(),Error>{
-        queue!(stdout(),Clear(ClearType::CurrentLine))?;
+    pub fn clear_line() -> Result<(), Error> {
+        queue!(stdout(), Clear(ClearType::CurrentLine))?;
         Ok(())
     }
-    pub fn move_cursor_to(position:Position) -> Result<(),Error>{
-        queue!(stdout(),MoveTo(position.x,position.y))?;
+    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
+        queue!(stdout(), MoveTo(position.x, position.y))?;
         Ok(())
     }
-    pub fn size()->Result<Size,Error>{
-        Ok(Size{
-            height:size()?.1,
-            width:size()?.0,
+    pub fn size() -> Result<Size, Error> {
+        Ok(Size {
+            height: size()?.1,
+            width: size()?.0,
         })
     }
-    pub fn hide_cursor() -> Result<(),Error>{
-        queue!(stdout(),Hide)?;
+    pub fn hide_cursor() -> Result<(), Error> {
+        queue!(stdout(), Hide)?;
         Ok(())
     }
-    pub fn show_cursor() -> Result<(),Error>{
-        queue!(stdout(),Show)?;
+    pub fn show_cursor() -> Result<(), Error> {
+        queue!(stdout(), Show)?;
         Ok(())
     }
-    pub fn print(content:&str)->Result<(),Error>{
-        queue!(stdout(),Print(content))?;
+    pub fn print(content: &str) -> Result<(), Error> {
+        queue!(stdout(), Print(content))?;
         Ok(())
     }
-    pub fn execute() -> Result<(),Error>{
+    pub fn print_center(content: &str, y1: u16) -> Result<(), Error> {
+        let x1 = Self::size()?.width / 2 - (content.len() as u16) / 2;
+        Self::move_cursor_to(Position { x: x1, y: y1 })?;
+        queue!(stdout(), Print(content))?;
+        Ok(())
+    }
+    pub fn execute() -> Result<(), Error> {
         //确保所有的输出都写入到输出设备
         stdout().flush()?;
         Ok(())
