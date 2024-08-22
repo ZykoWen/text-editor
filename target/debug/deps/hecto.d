eb02284b0097fc95ac186d3b82dbab6e56734797
Zyko <2049973245@qq.com>
2024-08-22 11:11:47 +0800

    assignment11--snap cursor to the end of the line

--- target/debug/deps/hecto.d
+++ target/debug/deps/hecto.d
@@ -1,12 +1,15 @@
-D:\rustProject\hecto\target\debug\deps\hecto.exe: src/main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs
+D:\rustProject\hecto\target\debug\deps\hecto.exe: src/main.rs src\editor.rs src\editor\editorcommand.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs src\editor\view\line.rs src\editor\view\location.rs
 
-D:\rustProject\hecto\target\debug\deps\hecto.d: src/main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs
+D:\rustProject\hecto\target\debug\deps\hecto.d: src/main.rs src\editor.rs src\editor\editorcommand.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs src\editor\view\line.rs src\editor\view\location.rs
 
 src/main.rs:
 src\editor.rs:
+src\editor\editorcommand.rs:
 src\editor\terminal.rs:
 src\editor\view.rs:
 src\editor\view\buffer.rs:
+src\editor\view\line.rs:
+src\editor\view\location.rs:
 
 # env-dep:CARGO_PKG_NAME=hecto
 # env-dep:CARGO_PKG_VERSION=0.1.0
